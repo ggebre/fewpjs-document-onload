0825101aecba04954d172e8e710db2d5a511c69c
Getu Gebre <getu.gebre.gg@gmail.com>
2021-01-07 17:34:15 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+let parag = document.getElementById("text");
+document.addEventListener("DOMContentLoaded", function() {
+        parag.innerHTML = "This is really cool!";
+  });
+  
+  
